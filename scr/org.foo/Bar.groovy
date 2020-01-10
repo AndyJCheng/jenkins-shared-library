e12843da6703ab1672c69897b619db552c4c1f40
andy cheng <you@example.com>
2020-01-10 22:34:34 +0800

    shared library

---
+++ scr/org.foo/Bar.groovy
@@ -0,0 +1,8 @@
+src/org.foo/Bar.groovy
+package org.foo
+def printName(name){
+    echo name
+
+}
+
+return this
---
+++ vars/foo.groovy
@@ -0,0 +1,10 @@
+vars/foo.groovy
+def printCommandName(commandName){
+    return {
+        echo 'the command name is:' + commandName
+    }
+}
+
+def test(){
+    echo 'this is pipeline shared library'
+}
