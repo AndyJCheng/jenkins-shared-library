bf7777f2e76888d01e16883ce7fc477673cb4ff8
AndyJCheng <25924057+AndyJCheng@users.noreply.github.com>
2024-07-15 20:13:49 +0800

    Update httpRquest.groovy

--- src/org/foo/httpRquest.groovy
+++ src/org/foo/httpRquest.groovy
@@ -1,22 +1,18 @@
 // send http request,form data
 
 def request()｛
-	     def formData = [
-                        [name: 'field1',value: 'value1'],
-                        [name: 'field2', value: 'value2'],
-                        [name: 'file1', file: 'path/to/your/file1.txt'],
-                        [name: 'file2', file: 'path/to/your/file2.txt']
-                    ]
-	def sendFormData(url, formData) {
-    def response = httpRequest(
-        url: url,
-        httpMode: 'POST',
-        contentType: 'MULTIPART_FORM_DATA',
-        multipartData: formData,
-        consoleLogResponseBody: true
-    )
-    return response
+	def formData = [
+		[name: 'field1',value: 'value1'],
+		[name: 'field2', value: 'value2'],
+		[name: 'file1', file: 'path/to/your/file1.txt'],
+		[name: 'file2', file: 'path/to/your/file2.txt']
+	 ]
+	def response = httpRequest(
+		url: url,
+		httpMode: 'POST',
+		contentType: 'MULTIPART_FORM_DATA',
+		multipartData: formData,
+		consoleLogResponseBody: true
+	)
+    	return response
 }
-
-       
-｝
